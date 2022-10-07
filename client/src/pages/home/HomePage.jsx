d30d6c52b00c965dde3f4b1de386072cf8a2ff44
Anar Seferov <anarsferov@gmail.com>
2022-10-07 10:48:01 +0400

    card added

---
+++ client/src/pages/home/HomePage.jsx
@@ -0,0 +1,12 @@
+import CardItem from "../../components/Card";
+
+function HomePage() {
+  return (
+    <div>
+      <nav>Header</nav>
+      <CardItem/>
+    </div>
+  );
+}
+
+export default HomePage;
