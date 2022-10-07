82446006badaad861e4cdb905e4e2e5080aa23f4
Anar Seferov <anarsferov@gmail.com>
2022-10-07 12:05:46 +0400

    style fix

--- client/src/pages/home/HomePage.jsx
+++ client/src/pages/home/HomePage.jsx
@@ -3,8 +3,8 @@ import { Col, Divider, Row } from "antd";
 
 function HomePage() {
   return (
-    <div style={{ marginTop: "3rem" }}>
-      <Row gutter={[16, 24]}>
+    <div style={{ marginTop: "3rem", width:"90%", margin:"2rem auto auto" }}>
+      <Row gutter={[24, 24]}>
         <Col className="gutter-row" span={6}>
           <CardItem />
         </Col>
