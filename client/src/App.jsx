aa8db038c0a5eb2d48ca72e3880e83e69e10c0d9
Nargiz Rafiyeva <nargiz.pr@code.local>
2022-10-07 10:58:20 +0400

    feat: store added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,18 +1,35 @@
 import { useState } from 'react';
 import {Route, Routes, useNavigate} from "react-router-dom";
-import Details from './pages/details'
+import DetailsPage from './pages/details'
 import AddAdvertisementPage from './pages/add-advertisement'
 import FavoritesPage from './pages/favorites'
-import HomePage from './pages/home'
+import HomePage from './pages/home';
+import { Breadcrumb, Layout, Menu } from 'antd';
+const { Header, Content, Footer } = Layout;
 
 function App() {
   return (
     <>
+    {/* <Header>
+      <div className="logo" />
+      <Menu
+        theme="dark"
+        mode="horizontal"
+        defaultSelectedKeys={['2']}
+        items={new Array(15).fill(null).map((_, index) => {
+          const key = index + 1;
+          return {
+            key,
+            label: `nav ${key}`,
+          };
+        })}
+      />
+    </Header> */}
       <Routes>
         <Route path="/" element={<HomePage />}/>
         <Route path="/favorites" element={<FavoritesPage />} />
         <Route path="/add-advertisement" element={<AddAdvertisementPage />} />
-        <Route path="/details/:id" element={<Details />} />
+        <Route path="/details/:id" element={<DetailsPage />} />
       </Routes>
     </>
   )
